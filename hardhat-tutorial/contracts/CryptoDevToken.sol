8686ac739fe05fa4768bb7b91e5456851fb65f17
AnicetFantomas <anicetmurhula20@gmail.com>
2023-03-07 14:26:39 +0200

    Update cryptoDevToken coontract

--- hardhat-tutorial/contracts/CryptoDevToken.sol
+++ hardhat-tutorial/contracts/CryptoDevToken.sol
@@ -44,6 +44,48 @@
 
        }
 
-       
+       function claim() public {
+        addrss sender = msg.sender;
 
+        // Get the number of CryptoDev NFT's held by a given sender address
+        uint256 balance = CryptoDevsNFT.balanceOf(sender);
+         // If the balance is zero, revert the transaction
+         require(balance > 0, "Your balance is too small for this transaction");
+         // amount keeps track of number of unclaimed tokenIds
+         uint256 amount = 0;
+         // loop over the balance and get the token ID owned by `sender` at a given `index` of its token list.
+
+         for (uint256 i = 0; i < balance; i++) {
+            uint256 tokenId = CryptoDevsNFT.tokenOfOwnerByIndex(sender, i);
+             // if the tokenId has not been claimed, increase the amount
+             if(tokenIdsClaimed[tokenId]) {
+                amount += 1;
+                tokenIdsClaimed[tokenId] = true;
+             }
+             // If all the token Ids have been claimed, revert the transaction;
+            require(amount > 0, "You have claimed all the tokens");
+            _mint(msg.sender, amount * tokensPerNFT);
+         }
+       }
+
+       /**
+        * @dev withdraws all ETH sent to this contract
+        * Requirements:
+        * wallet connected must be owner's address
+        */
+
+        function withdraw() public onlyOwner {
+            uint256 amount = address(this).balance;
+            require(amount > 0, "nothing to withdraw, account is empty");
+
+            address _owner = owner();
+            (bool sent,) = owner.call{value: amount}("");
+            require(sent, "Failed to send Ether");
+        }
+
+        // Function to receive Ether. msg.data must be empty
+      receive() external payable {}
+
+      // Fallback function is called when msg.data is not empty
+      fallback() external payable {}
   }
